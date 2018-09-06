45f3d6a0be7f2f8f49c099d0a369808da329b30c
alagarsamya8870 <42169898+alagarsamya8870@users.noreply.github.com>
2018-09-06 11:21:08 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class num1
+{
+public static void main(String arg[])
+{
+int a;
+Scanner s=new Scanner(System.in);
+System.out.println("elements are");
+a=s.nextInt();
+if(a%2==0)
+{
+System.out.println("even");
+}
+else if(a%2==1)
+{
+System.out.println("odd");
+}
+else
+{
+System.out.println("invalid");
+}
+}
+}
+
